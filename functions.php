3293daeb01205b3db12abd7348fba148d7f6426b
Eiji Kuroda <eiji@kuroda.me>
2012-09-18 17:23:28 +0900

    記事ページのデザインを作成。

--- functions.php
+++ functions.php
@@ -1,2 +1,11 @@
 <?php
-?>
+if ( function_exists('register_sidebar') ){
+    register_sidebar(array(
+	'description' => __( 'The primary widget area', 'dir' ),
+	'before_widget' => '<div id="%1$s" class="col2 widget %2$s">',
+	'after_widget' => "</div>",
+	'before_title' => '<h2 class="widget-title">',
+	'after_title' => '</h2>',
+    ));
+}
+ ?>
--- page_top.php
+++ page_top.php
@@ -1,17 +1,16 @@
-
+<?php
+/*
+Template Name: TOP
+*/
+?>
 <?php get_header(); ?>
 
 
 <?php if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) : the_post(); ?>
-		<div class="main_contents">
-			<h2 class="entry-title"><?php the_title(); ?></h2>
-			<div class="entry-content">
+			<h2><?php the_title(); ?></h2>
 			<?php the_content(); ?>
-		</div>
-		</div>
 	<?php endwhile; ?>
 <?php endif; ?>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
