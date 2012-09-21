99e70d6a63cda59ceeb8105d6a5b63884be833cc
Eiji Kuroda <eiji@kuroda.me>
2012-09-21 17:38:03 +0900

    change css

--- footer.php
+++ footer.php
@@ -2,12 +2,15 @@
 </div>
 <!--******************end contents-->
 
-
 <!--******************footer-->
 <div id="footer">
 <p class="copyright">Copyright (C) Flores International.All Rights Reserved.</p>
 </div>
 <!--******************end footer-->
 <?php wp_footer(); ?>
+
+
+</div>
+<!--******************end wrapper-->
 </body>
 </html>
